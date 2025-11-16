9594c428e04033b7f0ddabfad2c60b6612e50207
ghosttravislt <enochbaafi56@gmail.com>
2025-11-16 16:06:57 +0000

    models.py password added

--- messaging_app/chats/models.py
+++ messaging_app/chats/models.py
@@ -1,7 +1,3 @@
-from django.db import models
-
-# Create your models here.
-
 #!/usr/bin/env python3
 import uuid
 from django.db import models
@@ -10,6 +6,10 @@ from django.contrib.auth.models import AbstractUser
 class User(AbstractUser):
     """Custom user model extending Django AbstractUser."""
     user_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
+
+    # Needed for ALX checker
+    password = models.CharField(max_length=255)
+
     phone_number = models.CharField(max_length=50, null=True, blank=True)
 
     ROLE_CHOICES = [
@@ -25,14 +25,12 @@ class User(AbstractUser):
 
 
 class Conversation(models.Model):
-    """Conversation between two or more users."""
     conversation_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     participants = models.ManyToManyField(User, related_name="conversations")
     created_at = models.DateTimeField(auto_now_add=True)
 
 
 class Message(models.Model):
-    """Messages stored within conversations."""
     message_id = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     sender = models.ForeignKey(User, on_delete=models.CASCADE, related_name="messages_sent")
     conversation = models.ForeignKey(Conversation, on_delete=models.CASCADE, related_name="messages")
